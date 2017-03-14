9d767528640befa054a42f2ee34c67a532b9d42b
AirEDoctor <airedoctor@liaorenandemacbook-pro.local>
2017-03-14 11:22:54 +0800

    Initial Commit

---
+++ AEPicekerViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AEPicekerViewExample
+//
+//  Created by AirEDoctor on 14/03/2017.
+//  Copyright © 2017 AirEDoctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AEPicekerViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AEPicekerViewExample
+//
+//  Created by AirEDoctor on 14/03/2017.
+//  Copyright © 2017 AirEDoctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
